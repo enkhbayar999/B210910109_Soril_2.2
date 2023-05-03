6e784b38899a59ee56000881b33b548aca684301
Admin <Admin@DESKTOP-LKF6Q46>
2023-05-03 14:31:41 +0800

    Turshilt 0.0.1

--- src/Immutable/Main.java
+++ src/Immutable/Main.java
@@ -5,7 +5,8 @@ public class Main {
 		Course course = new Course("Software Construction"); 
 		course.setCourseName("Software Construction");
 		
-//		System.out.println(course1.courseName); 
+//		System.out.println(course1.courseName); Uuchlaarai bagshaa <3
+		//Turshilt 0.0.1
 		System.out.println(course.getCourseName());
 		
 		course.setCourseName("Программ хангамжийн бүтээлт"); 
